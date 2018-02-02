cf61b63aefff78440423cf73b17db1d7b53ba698
Zzy <zhengziyuan@pku.edu.cn>
2018-02-02 22:19:53 +0800

    add time gap to each sending

--- processfile.sh
+++ processfile.sh
@@ -8,7 +8,7 @@ rip2=192.168.2.12
 filepath=/home/ofcl/Documents/hualong/transmissionProject/uploadServer/uploadFiles/
 #savepath=/home/hualong2/Documents/hualong/server/saveFiles/
 flute1="$flutepath -S -k:2 -m:192.168.1.10 -p:4000 -t:2 -i:192.168.1.13 -r:5000000 -n:1 -F:"
-flute2="$flutepath -S -k:2 -m:192.168.2.10 -p:4000 -t:2 -i:192.168.2.12 -r:5000000 -n:1 -F:"
+flute2="$flutepath -S -k:2 -m:192.168.2.10 -p:4001 -t:2 -i:192.168.2.12 -r:5000000 -n:1 -F:"
 
 function send_withflute1(){
 	if [ "$(ls -A $filepath)" ]; then
@@ -22,7 +22,9 @@ function send_withflute1(){
 			if [ -n $ison_flute1 ]; then
 			#($flutepath -S -m:192.168.1.10 -p:4000 -t:2 -i:192.168.1.13 -r:5000000 -n:1 -F:$entry)&&(mv $entry $entry.sent) &	
 			#$flutepath -S -m:${sip1} -p:4000 -t:2 -i:${rip1} -r:5000000 -n:1 -F:${entry} &&(mv $entry $entry.sent) &
-			(${flute1}${entry})&&(mv $entry $entry.sent) &
+			${flute1}${entry}
+			sleep 1
+			mv $entry $entry.sent
 			fi
 		fi
 		done
@@ -42,7 +44,9 @@ function send_withflute2(){
 			if [ -n $ison_flute2 ]; then
 			#($flutepath -S -m:192.168.1.10 -p:4000 -t:2 -i:192.168.1.13 -r:5000000 -n:1 -F:$entry)&&(mv $entry $entry.sent) &	
 			#$flutepath -S -m:${sip1} -p:4000 -t:2 -i:${rip1} -r:5000000 -n:1 -F:${entry} &&(mv $entry $entry.sent) &
-			(${flute2}${entry})&&(mv $entry $entry.sent) &
+			${flute2}${entry}
+			sleep 1 
+			mv $entry $entry.sent
 			fi
 		fi
 		done
@@ -62,12 +66,16 @@ function send_withbothflute(){
 			if [ -n $ison_flute1 ]; then
 			#($flutepath -S -m:192.168.1.10 -p:4000 -t:2 -i:192.168.1.13 -r:5000000 -n:1 -F:$entry)&&(mv $entry $entry.sent) &	
 			#$flutepath -S -m:${sip1} -p:4000 -t:2 -i:${rip1} -r:5000000 -n:1 -F:${entry} &&(mv $entry $entry.sent) &
-			(${flute1}${entry})&&(mv $entry $entry.sent) &
+			${flute1}${entry}
+			sleep 1
+			mv $entry $entry.sent
 			else
 				ison_flute2=`ps -ef|grep $sip2|grep -v grep|awk '{print $2}'`
 				if [ -n $ison_flute2 ]; then
 					#($flute -S -m:$sip2 -p:4000 -t:2 -i:$rip2 -r:5000000 -n:1 -F:$entry)&&(mv $entry $entry.sent) &
-					(${flute2}${entry})&&(mv $entry $entry.sent) &
+					${flute2}${entry}
+					sleep 1
+					mv $entry $entry.sent
 				fi
 			fi
                #$flute -S -m:192.168.1.10 -p:4000 -t:2 -i:192.168.1.13 -r:5000000 -n:1 -F:$entry
@@ -82,15 +90,15 @@ while [ 1 ]
 do
 	case $tube_number in
 		1)
-		echo "use flute1"
+		#echo "use flute1"
 		send_withflute1
 		;;
 		2)
-		echo "use flute2"
+		#echo "use flute2"
 		send_withflute2
 		;;
 		0)
-		echo "use both flutes"
+		#echo "use both flutes"
 		send_withbothflute
 		;;
 		*)
